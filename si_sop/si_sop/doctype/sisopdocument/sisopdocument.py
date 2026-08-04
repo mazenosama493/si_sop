0681ad8869c3bca697b24bb35d9b4b76aee9a089
mazenosama493 <mazenosama987654321@gmail.com>
2026-08-04 08:58:22 +0300

    fix bugs and add validations

--- si_sop/si_sop/doctype/sisopdocument/sisopdocument.py
+++ si_sop/si_sop/doctype/sisopdocument/sisopdocument.py
@@ -1,9 +1,50 @@
-# Copyright (c) 2026, volt and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class SiSopDocument(Document):
-	pass
+
+    def validate(self):
+        self.validate_document_type()
+        self.validate_dates()
+        self.validate_departments()
+        self.validate_users()
+        self.validate_content()
+        self.validate_approved_person()
+
+		
+    def validate_approved_person(self):
+        if self.status != "Approved":
+            return
+
+        approved_users = [user for user in self.users if user.role=="Approved By"]
+
+        if len(approved_users) == 0:
+            frappe.throw(
+                _("An approved document must have one approved person.")
+            )
+
+    def validate_document_type(self):
+        if self.document_type not in ("SOP", "SI"):
+            frappe.throw(_("Document Type must be SOP or SI."))
+
+    def validate_dates(self):
+        if self.issue_date and self.approval_date:
+            if self.approval_date < self.issue_date:
+                frappe.throw(_("Approval Date cannot be before Issue Date."))
+
+        if self.status == "Approved" and not self.approval_date:
+            frappe.throw(_("Approval Date is mandatory for approved documents."))
+
+    def validate_departments(self):
+        if not self.departments:
+            frappe.throw(_("Please add at least one Department."))
+
+    def validate_users(self):
+        if not self.users:
+            frappe.throw(_("Please add at least one User."))
+
+    def validate_content(self):
+        if self.status == "Approved" and not self.content:
+            frappe.throw(_("Content is mandatory before approving the document."))
